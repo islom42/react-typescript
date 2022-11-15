affc88c472a066f5b9736d5f07d9e5ee33381cd8
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-15 22:57:04 +0300

    feat: Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
     return(
         // <h1>Hello world</h1>
         e("div", {className: "container"},[
-            e("h1",{className: "font-bold"}, "Text Test")
-    ], e("button", {className: "btn"}, "Click me"))
+            e("h1",{className: "font-bold"}, "Text Test"),
+            e("button", {className: "py-2 px-4 border btn"}, "Click me")
+    ])
     )
 }
 export default App
