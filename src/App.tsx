383b7749fbef795b966c4b652c36f8ee42365847
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-15 22:37:34 +0300

    feat: Add syntax sugar of jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {createElement as e} from "react";
 
 function App() {
     return(
-        <h1>Hello world</h1>
+        // <h1>Hello world</h1>
+        e("div", {className: "container"},[
+            e("h1",{className: "font-bold"}, "Text Test")
+    ], e("button", {className: "btn"}, "Click me"))
     )
 }
 export default App
